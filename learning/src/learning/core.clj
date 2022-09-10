b02c1a61da549203004a4485e96e480108ee98fd
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-10 22:18:24 +0600

    Import namespace and use funtion from diff namespace

--- learning/src/learning/core.clj
+++ learning/src/learning/core.clj
@@ -1,14 +1,14 @@
 (ns learning.core
+  (:require [learning.list :as list])
   (:gen-class))
 
-(defn greeting [name] 
+(defn greeting [name]
   (str "Hello " name))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println (greeting "Hasan"))
-  ;; (println (ls.list.make-double (range 10)))
   ;; execute code in reverse way using pipe operator
   (->> (range 10) (map #(* 2 %)) (println))
-)
+  (println (list/add [1 2 3 44])))
--- learning/src/learning/list.clj
+++ learning/src/learning/list.clj
@@ -1,4 +1,5 @@
-(ns learning.ls)
+(ns learning.list)
 
 (def add (fn [nums] (reduce + nums)))
 (defn make-double [nums] (map #(* % 2) nums))
+(def words ["accident" "initiative" "clothes" "library" "cancer" "mixture" "aspect" "bathroom" "photo" "cousin" "recipe" "speech" "story" "debt" "engine" "direction" "tooth" "interaction" "river" "magazine" "thing" "internet" "awareness" "desk" "math" "role" "ratio" "negotiation" "grandmother" "statement"])
