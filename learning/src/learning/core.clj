25416ecd6640fcb18a178ff1cf09b16d6a8335cd
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-10 17:59:49 +0600

    Practice string method

--- learning/src/learning/core.clj
+++ learning/src/learning/core.clj
@@ -1,7 +1,9 @@
 (ns learning.core
   (:gen-class))
 
+(defn greeting [name] 
+  (str "Hello " name))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (greeting "Hasan")))
