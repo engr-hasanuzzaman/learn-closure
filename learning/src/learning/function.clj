ec696e267e62d3ee1193fe3565d028f5379a9b23
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-18 08:34:32 +0600

    Practice variadic function

--- learning/src/learning/function.clj
+++ learning/src/learning/function.clj
@@ -1,4 +1,4 @@
-;; Multi-arity functions
+;; Multi-arity functions (method overloading)
 (defn message
 ;; without any param   
   ([] (prn "Good Bye!"))
@@ -7,3 +7,10 @@
 
 (message)
 (message "See you again!")
+
+;; Variadic functions (function that take variable number of parameters)
+;; variable number of paramerters will be place at the end of parameter
+(defn wellcome [msg & persons]
+  (print msg (clojure.string/join ", " persons)))
+
+(wellcome "Wellcome to Clojure world" "hasan" "zaman")
