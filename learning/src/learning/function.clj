e10dd28394156dbc170cc7b3b7099852477c8df5
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-18 08:47:49 +0600

    Practive annonymous method

--- learning/src/learning/function.clj
+++ learning/src/learning/function.clj
@@ -11,6 +11,21 @@
 ;; Variadic functions (function that take variable number of parameters)
 ;; variable number of paramerters will be place at the end of parameter
 (defn wellcome [msg & persons]
-  (print msg (clojure.string/join ", " persons)))
+  (println msg (clojure.string/join ", " persons)))
 
 (wellcome "Wellcome to Clojure world" "hasan" "zaman")
+
+;; annonymous function
+;; full version
+(defn greeting [msg name]
+  (println msg, name))
+
+;; annonymous version
+(def greeting1 (fn [msg name] (println msg, name)))
+
+;; shorter annonymous function
+(def greeting2 #(println %1 %2))
+
+(greeting "Wellcome" "hasan")
+(greeting1 "Wellcome" "hasan")
+(greeting2 "Wellcome" "hasan")
