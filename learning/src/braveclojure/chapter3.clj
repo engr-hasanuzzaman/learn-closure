108e53c3b916b3054c1049c3727fd314281771a3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-20 10:51:52 +0600

    Practive problem chapter 3

--- learning/src/braveclojure/chapter3.clj
+++ learning/src/braveclojure/chapter3.clj
@@ -7,4 +7,26 @@
 (def man (hash-map :name "sumon" :age 32))
 (def skill (hash-set :ruby :clojure :javaScript :ruby))
 
-(println v l man skill)
+;; -----------2nd problem (Write a function that takes a number and adds 100 to it.)
+(defn second-solution []
+  (println "After adding 10 with 100" (+ 10 100)))
+
+;; ----------- 3rd problem (Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:)
+(defn dec-maker [num]
+  (- num 1))
+
+(defn third-solution []
+  (println "use dec-maker with 100 is: " (dec-maker 100)))
+
+;; function to call particular problem
+(defn first-solution []
+  (println v l man skill))
+
+
+;; run the solution
+(println "->first solution")
+(first-solution)
+(println "->2nd solution")
+(second-solution)
+(println "->3rd solution")
+(third-solution)
