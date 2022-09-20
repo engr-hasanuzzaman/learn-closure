4c2e489e2dc71fb68c4ca8524ac346e355f06b69
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-20 12:58:33 +0600

    Note about how map works and use of seq method

--- learning/src/learning/core.clj
+++ learning/src/learning/core.clj
@@ -12,3 +12,13 @@
   ;; execute code in reverse way using pipe operator
   (->> (range 10) (map #(* 2 %)) (println))
   (println (list/add [1 2 3 44])))
+
+;; how map work on list, map, vector and set
+;; 1. It use seq method to convert the input with has first, rest & cons interface
+;; then use these three method to map and apply transformation
+(prn "Sequence example")
+(seq {:name "sumon" :age 32 :phone "012333"})
+(seq `(1 2 3 4 45 5))
+(seq [1 2 3 4 45 5])
+(seq #{:name :age 23 1 2 3 4 5})
+
