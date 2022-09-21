d68b21abc8031c5d140bfb61f3bd37858958c693
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-09-22 00:13:41 +0600

    Create map using shorthand function

--- learning/src/braveclojure/chapter4.clj
+++ learning/src/braveclojure/chapter4.clj
@@ -2,4 +2,6 @@
 (defn red-map [f lst]
   (reduce (fn [acc item] (conj acc (f item))) `() (reverse lst)))
 
-;; 
+;; with shorthand function
+(defn red-map-2 [f lst]
+  (reduce #(conj %1 (f %2)) `() (reverse lst)))
